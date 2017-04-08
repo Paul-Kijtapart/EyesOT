d356a7d3691742c304489b26b21403898b17d9b5
Paul-Kijtapart <scc385@gmail.com>
2017-04-08 16:04:31 -0700

    React work with chat app

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import io from 'socket.io-client';
 
-$(function() {
-    var socket = io();
-    $('form').submit(function() {
+let socket = io('http://localhost:3000');
+
+// $(function() {
+//     var socket = io();
+//     $('form').submit(function() {
+//         socket.emit('chat message', $('#m').val());
+//         $('#m').val('');
+//         return false;
+//     });
+//     socket.on('chat message', function(msg) {
+//         $('#messages').append($('<li>').text(msg));
+//     });
+// });
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        socket.on("chat message", function(msg) {
+            $('#messages').append($('<li>').text(msg));
+        });
+    }
+
+    onFormSubmit(e) {
+        e.preventDefault();
         socket.emit('chat message', $('#m').val());
         $('#m').val('');
         return false;
-    });
-    socket.on('chat message', function(msg) {
-        $('#messages').append($('<li>').text(msg));
-    });
-});
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <ul id="messages"></ul>
+                <form onSubmit={this.onFormSubmit} action="">
+                <input id="m" autocomplete="off" />
+                    <button>Send</button>
+                </form>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-    <div> HIHIHIHIHI</div>,
+    <App />,
     document.getElementById("root")
 );
